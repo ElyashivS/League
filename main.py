5ef5948d7dc69c7b72244f5f87e3280943165b79
Elyashiv <Elyashiv2468@gmail.com>
2022-04-29 12:15:07 +0300

    Added endTimeBet to all objects, canceled running loop, get information from JSON everytime. Got to do this more efficient.

--- main.py
+++ main.py
@@ -4,101 +4,103 @@ import json
 
 HomeTeam = "Home Team"
 AwayTeam = "Away Team"
-endTimeBet = datetime.time.max.strftime("%H:%M:%S")
 writeToTable = ""
-welcomeLine = ""
 bets = ""
+abc = json.load(open('data.json'))
+# print(abc[2]['points'])
 
-Elyashiv = Player("Elyashiv", 0, 0, 0, 0, 0)
-David = Player("David", 0, 0, 0, 0, 0)
-Yuval = Player("Yuval", 0, 0, 0, 0, 0)
-Yehiam = Player("Yehiam", 0, 0, 0, 0, 0)
-Matanel = Player("Matanel", 0, 0, 0, 0, 0)
-Shmuel = Player("Shmuel", 0, 0, 0, 0, 0)
+Elyashiv = Player("Elyashiv", 0, 0, 0, 0, 0, datetime.time.max.strftime("%H:%M:%S"))
+David = Player("David", 0, 0, 0, 0, 0, None)
+Yuval = Player("Yuval", 0, 0, 0, 0, 0, None)
+Yehiam = Player("Yehiam", 0, 0, 0, 0, 0, None)
+Matanel = Player("Matanel", 0, 0, 0, 0, 0, None)
+Shmuel = Player("Shmuel", 0, 0, 0, 0, 0, None)
 
 players = [Elyashiv, David, Yuval, Yehiam, Matanel, Shmuel]
-running = True
+# running = True
 
-while running:
-    choice = 1
-    welcomeLine = """Welcome to the Tribuna league!
+# while running:
+choice = 1
+welcomeLine = """Welcome to the Tribuna league!
 +---------+----------------+--------------------+----------+
 |  Name   |  Exact result  |  Winning identity  |   PTS    |
 +=========+================+====================+==========+"""
 
-    players.sort(key=lambda x: x.get_points(), reverse=True)
+players.sort(key=lambda x: x.get_points(), reverse=True)
 
-    for i in players:
-        writeToTable += i.get_stats() + "\n"
-    print(welcomeLine)
-    print(writeToTable)
+for i in players:
+    writeToTable += i.get_stats() + "\n"
+print(welcomeLine)
+print(writeToTable)
 
-    print("""Please enter your number:
+print("""Please enter your number:
     1. Elyashiv
     2. David
     3. Yuval
     4. Yehiam
     5. Matanel
     6. Shmuel""")
-    name = int(input("\t"))
-    if name == 1:
-        name = Elyashiv
-        choice = int(input("""\t1. Bet
+name = int(input("\t"))
+if name == 1:
+    name = Elyashiv
+    choice = int(input("""\t1. Bet
     2. Enter result of a game
     3. Enter next game teams
     4. Enter end time of betting
     """))
-        if choice == 1:
-            pass
-        elif choice == 2:
-            RHome = int(input("Enter score of HOME team: "))
-            RAway = int(input("Enter score of AWAY team: "))
-            for i in players:
-                if i.get_home_bet() == RHome and i.get_away_bet() == RAway:
-                    i.set_exact_result(i.get_exact_result() + 1)
-                    i.set_points(i.get_points() + 3)
-                    print(i.get_name(), "bet", i.get_home_bet(), "-", i.get_away_bet(), "and got exact result!"
-                                                                                        " good job. 3 points :)")
-                elif (i.get_home_bet() > i.get_away_bet() and RHome > RAway) or \
-                        (i.get_home_bet() < i.get_away_bet() and RHome < RAway) or \
-                        (i.get_home_bet() == i.get_away_bet() and RHome == RAway):
-                    i.set_winning_identity(i.get_winning_identity() + 1)
-                    i.set_points(i.get_points() + 1)
-                    print(i.get_name(), "bet", i.get_home_bet(), "-", i.get_away_bet(), "and got identity. 1 point.")
-                else:
-                    print(i.get_name(), "bet", i.get_home_bet(), "-", i.get_away_bet(), "and got nothing :D")
-
-        elif choice == 3:
-            HomeTeam = input("\tEnter the name of the HOME team: ")
-            AwayTeam = input("\tEnter the name of the AWAY team: ")
-        elif choice == 4:
-            hour, minutes = int(input("Enter a hour: ")), int(input("Enter a minutes: "))
-            endTimeBet = datetime.time(hour, minutes).strftime("%H:%M:%S")
-            print("The end time of the bet has updated to", endTimeBet)
-    elif name == 2:
-        name = David
-    elif name == 3:
-        name = Yuval
-    elif name == 4:
-        name = Yehiam
-    elif name == 5:
-        name = Matanel
-    elif name == 6:
-        name = Shmuel
     if choice == 1:
-        print("Hello," + name.get_name() + "!\nBet is open until " + str(endTimeBet))
-        now = datetime.datetime.now().strftime("%H:%M:%S")
-        if now < endTimeBet:
-            name.set_home_bet(int(input("Please enter the number of goals for " + HomeTeam + ": ")))
-            name.set_away_bet(int(input("Please enter the number of goals for " + AwayTeam + ": ")))
-            # print("Your bet is:", HomeTeam, name.get_home_bet(), "-", name.get_away_bet(), AwayTeam)
-            bets += "\n" + name.get_name() + " bet is: " + str(HomeTeam) + " " + str(name.get_home_bet()) \
-                    + " - " + str(name.get_away_bet()) + " " + str(AwayTeam)
-            print(bets)
+        pass
+    elif choice == 2:
+        RHome = int(input("Enter score of HOME team: "))
+        RAway = int(input("Enter score of AWAY team: "))
+        for i in players:
+            if i.get_home_bet() == RHome and i.get_away_bet() == RAway:
+                i.set_exact_result(i.get_exact_result() + 1)
+                i.set_points(i.get_points() + 3)
+                print(i.get_name(), "bet", i.get_home_bet(), "-", i.get_away_bet(), "and got exact result!"
+                                                                                    " good job. 3 points :)")
+            elif (i.get_home_bet() > i.get_away_bet() and RHome > RAway) or \
+                    (i.get_home_bet() < i.get_away_bet() and RHome < RAway) or \
+                    (i.get_home_bet() == i.get_away_bet() and RHome == RAway):
+                i.set_winning_identity(i.get_winning_identity() + 1)
+                i.set_points(i.get_points() + 1)
+                print(i.get_name(), "bet", i.get_home_bet(), "-", i.get_away_bet(), "and got identity. 1 point.")
+            else:
+                print(i.get_name(), "bet", i.get_home_bet(), "-", i.get_away_bet(), "and got nothing :D")
 
-        else:
-            print("Sorry, " + name.get_name() + " time of bet has passed.")
-        running = False
+    elif choice == 3:
+        HomeTeam = input("\tEnter the name of the HOME team: ")
+        AwayTeam = input("\tEnter the name of the AWAY team: ")
+    elif choice == 4:
+        hour, minutes = int(input("Enter a hour: ")), int(input("Enter a minutes: "))
+        abc[0]['endTimeBet'] = datetime.time(hour, minutes).strftime("%H:%M:%S")
+
+        print("The end time of the bet has updated to", abc[0]['endTimeBet'])
+
+elif name == 2:
+    name = David
+elif name == 3:
+    name = Yuval
+elif name == 4:
+    name = Yehiam
+elif name == 5:
+    name = Matanel
+elif name == 6:
+    name = Shmuel
+if choice == 1:
+    print("Hello," + name.get_name() + "!\nBet is open until " + str(abc[0]['endTimeBet']))
+now = datetime.datetime.now().strftime("%H:%M:%S")
+if now < abc[0]['endTimeBet']:
+    name.set_home_bet(int(input("Please enter the number of goals for " + HomeTeam + ": ")))
+    name.set_away_bet(int(input("Please enter the number of goals for " + AwayTeam + ": ")))
+    # print("Your bet is:", HomeTeam, name.get_home_bet(), "-", name.get_away_bet(), AwayTeam)
+    bets += "\n" + name.get_name() + " bet is: " + str(HomeTeam) + " " + str(name.get_home_bet()) \
+            + " - " + str(name.get_away_bet()) + " " + str(AwayTeam)
+    print(bets)
+
+else:
+    print("Sorry, " + name.get_name() + " time of bet has passed.")
+running = False
 
 j = json.dumps([o.dump() for o in players])
 
@@ -107,14 +109,4 @@ f.write(j)
 f.close()
 
 abc = json.load(open('data.json'))
-print(abc[2]['points'])
-
-# abc = json.load(open('data.json'))
-# print(abc['awayBet'])
-#
-# y = Elyashiv.toJSON()
-# j = json.dumps(y)
-#
-# f = open("data.json", "w")
-# f.write(y)
-# f.close()
+print(abc[2]['points'], "Json WORKS")
